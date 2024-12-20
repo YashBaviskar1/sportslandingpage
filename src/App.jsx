113e2a1baa977138a49f16cbcd0e30f32c81e1ba
Yadnesh Dinesh Bamne <110719904+YadneshBamne@users.noreply.github.com>
2024-12-20 20:10:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,10 @@ import {
 import Sports from './Sports/Sports';
 import Cultural from './Cultural/Cultural';
 import Events from './Cultural/Events';
-import Schedule from './Sports/Schedule';
+import Login from './Sports/Login';
+import Schedule from "./Sports/Schedule"
+import Council from './Council/Council';
+import Heads from './Heads/Heads';
 
 function App() {
   return (
@@ -18,7 +21,14 @@ function App() {
       <Route path="/sports" exact element={<Sports />} />
       <Route path="/cultural" exact element={<Cultural />} />
       <Route path="/events" exact element={<Events />} />
+      <Route path="/login" exact element={<Login />} />
       <Route path="/schedule" exact element={<Schedule />} />
+      <Route path="/council" exact element={<Council />} />
+      <Route path="/heads" exact element={<Heads />} />
+
+
+
+      
     </Routes>
     </Router>
   )
