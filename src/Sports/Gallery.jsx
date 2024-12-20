cfe3e32890a73c0f92e47840a40bdc250d6aaffd
Yadnesh Dinesh Bamne <110719904+YadneshBamne@users.noreply.github.com>
2024-12-20 20:14:09 +0530

    Update Gallery.jsx

--- src/Sports/Gallery.jsx
+++ src/Sports/Gallery.jsx
@@ -1,67 +1,46 @@
-import React, { useEffect, useRef, useState } from "react";
-import "./Gallery.css";
-import image1 from "../assets/image1.JPG";
-import image2 from "../assets/image2.JPG";
-import image3 from "../assets/image3.JPG";
-import image4 from "../assets/image4.JPG";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ResponsiveGallery from 'react-responsive-gallery';
 
-const images = [
-  image1,
-  image2,
-  image3,
-  image4
+const media = [
+  {
+    src: 'https://res.cloudinary.com/dyhby7bo9/image/upload/v1734699275/OJUSPHOTOS/rby01igc0oksukw45o0w.jpg',
+  },
+  {
+    src: 'https://res.cloudinary.com/dyhby7bo9/image/upload/v1734699294/OJUSPHOTOS/krxbvxrvomputibc8xhg.jpg',
+  },
+  {
+    src: 'https://cdn.pixabay.com/photo/2016/12/04/21/58/rabbit-1882699_960_720.jpg',
+  },
+  {
+    src: 'https://cdn.pixabay.com/photo/2014/07/08/12/36/bird-386725_960_720.jpg',
+  },
+  {
+    src: 'https://cdn.pixabay.com/photo/2015/10/12/15/46/fallow-deer-984573_960_720.jpg',
+  },
+  {
+    src: 'https://cdn.pixabay.com/photo/2014/10/01/10/44/hedgehog-468228_960_720.jpg',
+  },
+  {
+    src: 'https://cdn.pixabay.com/photo/2013/09/22/15/29/prairie-dog-184974_960_720.jpg',
+  },
+  {
+    src: 'https://cdn.pixabay.com/photo/2018/03/31/06/31/dog-3277416_960_720.jpg',
+  },
+  {
+    src: 'https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313_960_720.jpg',
+  },
+  {
+    src: 'https://cdn.pixabay.com/photo/2019/03/09/17/30/horse-4044547_960_720.jpg',
+  },
 ];
 
-
-const Gallery = () => {
-  const galleryRef = useRef(null);
-
-  useEffect(() => {
-    const gallery = galleryRef.current;
-
-    const scrollGallery = () => {
-      if (gallery) {
-        if (gallery.scrollLeft >= gallery.scrollWidth - gallery.clientWidth) {
-          gallery.scrollLeft = 0; // Reset to start
-        } else {
-          gallery.scrollBy({ left: 1, behavior: "smooth" });
-        }
-      }
-    };
-
-    const interval = setInterval(scrollGallery, 20); // Adjust speed as needed
-
-    return () => clearInterval(interval); // Cleanup interval on component unmount
-  }, []);
-
+const GalleryComponent = () => {
   return (
-    <div className="gallery-container">
-      <div className="gallery">
-        <img
-          src="https://images.unsplash.com/photo-1455368109333-ebc686ad6c58?q=80&w=1476&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt=""
-        />
-        <img
-          src="https://plus.unsplash.com/premium_photo-1664528201657-378dbf6a4a91?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt=""
-        />
-        <img
-          src="https://images.unsplash.com/photo-1516651000622-7f32fe80a57a?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt=""
-        />
-        <img
-          src="https://images.unsplash.com/photo-1435348773030-a1d74f568bc2?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt=""
-        />
-      </div>
+    <div>
+      <ResponsiveGallery media={media} />
     </div>
   );
 };
 
-export default Gallery;
-
-
-
-
-
-
+export default GalleryComponent;
