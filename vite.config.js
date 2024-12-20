518a94c28a7af06e5284b595c9e12f9b915bb49c
Yadnesh Dinesh Bamne <110719904+YadneshBamne@users.noreply.github.com>
2024-12-20 20:23:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+   build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'locomotive-scroll': ['locomotive-scroll'],
+        },
+      },
+    },
+  },
   plugins: [react()],
   assetsInclude: ['**/*.JPG', '**/*.JPEG'],
   resolve: {
