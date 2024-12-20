cea4706b579e76537cfb45b126279a9420767c6a
Yadnesh Dinesh Bamne <110719904+YadneshBamne@users.noreply.github.com>
2024-12-20 20:33:30 +0530

    Update Heads.jsx

--- src/Heads/Heads.jsx
+++ src/Heads/Heads.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import ScrollReveal from 'scrollreveal';
 import './Heads.css'; // assuming heads.css is in your src folder
 
 const Heads = () => {
@@ -8,15 +7,6 @@ const Heads = () => {
     window.scrollTo(0, 0);
   };
 
-  useEffect(() => {
-    // ScrollReveal animations
-    ScrollReveal({
-      distance: '60px',
-      duration: 2500,
-      delay: 400
-    });
-    ScrollReveal().reveal('h2', { delay: 100, origin: 'top' });
-    ScrollReveal().reveal('#team', { delay: 500, origin: 'bottom' });
 
     // IntersectionObserver to highlight the nav items based on section visibility
     const sections = document.querySelectorAll('section');
