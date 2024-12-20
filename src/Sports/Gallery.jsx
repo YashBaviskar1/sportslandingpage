bd7c796ae8611ad88e17c8ce4ea12470d508aec2
Yadnesh Dinesh Bamne <110719904+YadneshBamne@users.noreply.github.com>
2024-12-20 20:38:27 +0530

    Update Gallery.jsx

--- src/Sports/Gallery.jsx
+++ src/Sports/Gallery.jsx
@@ -1,46 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import ResponsiveGallery from 'react-responsive-gallery';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import ResponsiveGallery from 'react-responsive-gallery';
 
-const media = [
-  {
-    src: 'https://res.cloudinary.com/dyhby7bo9/image/upload/v1734699275/OJUSPHOTOS/rby01igc0oksukw45o0w.jpg',
-  },
-  {
-    src: 'https://res.cloudinary.com/dyhby7bo9/image/upload/v1734699294/OJUSPHOTOS/krxbvxrvomputibc8xhg.jpg',
-  },
-  {
-    src: 'https://cdn.pixabay.com/photo/2016/12/04/21/58/rabbit-1882699_960_720.jpg',
-  },
-  {
-    src: 'https://cdn.pixabay.com/photo/2014/07/08/12/36/bird-386725_960_720.jpg',
-  },
-  {
-    src: 'https://cdn.pixabay.com/photo/2015/10/12/15/46/fallow-deer-984573_960_720.jpg',
-  },
-  {
-    src: 'https://cdn.pixabay.com/photo/2014/10/01/10/44/hedgehog-468228_960_720.jpg',
-  },
-  {
-    src: 'https://cdn.pixabay.com/photo/2013/09/22/15/29/prairie-dog-184974_960_720.jpg',
-  },
-  {
-    src: 'https://cdn.pixabay.com/photo/2018/03/31/06/31/dog-3277416_960_720.jpg',
-  },
-  {
-    src: 'https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313_960_720.jpg',
-  },
-  {
-    src: 'https://cdn.pixabay.com/photo/2019/03/09/17/30/horse-4044547_960_720.jpg',
-  },
-];
+// const media = [
+//   {
+//     src: 'https://res.cloudinary.com/dyhby7bo9/image/upload/v1734699275/OJUSPHOTOS/rby01igc0oksukw45o0w.jpg',
+//   },
+//   {
+//     src: 'https://res.cloudinary.com/dyhby7bo9/image/upload/v1734699294/OJUSPHOTOS/krxbvxrvomputibc8xhg.jpg',
+//   },
+//   {
+//     src: 'https://cdn.pixabay.com/photo/2016/12/04/21/58/rabbit-1882699_960_720.jpg',
+//   },
+//   {
+//     src: 'https://cdn.pixabay.com/photo/2014/07/08/12/36/bird-386725_960_720.jpg',
+//   },
+//   {
+//     src: 'https://cdn.pixabay.com/photo/2015/10/12/15/46/fallow-deer-984573_960_720.jpg',
+//   },
+//   {
+//     src: 'https://cdn.pixabay.com/photo/2014/10/01/10/44/hedgehog-468228_960_720.jpg',
+//   },
+//   {
+//     src: 'https://cdn.pixabay.com/photo/2013/09/22/15/29/prairie-dog-184974_960_720.jpg',
+//   },
+//   {
+//     src: 'https://cdn.pixabay.com/photo/2018/03/31/06/31/dog-3277416_960_720.jpg',
+//   },
+//   {
+//     src: 'https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313_960_720.jpg',
+//   },
+//   {
+//     src: 'https://cdn.pixabay.com/photo/2019/03/09/17/30/horse-4044547_960_720.jpg',
+//   },
+// ];
 
-const GalleryComponent = () => {
-  return (
-    <div>
-      <ResponsiveGallery media={media} />
-    </div>
-  );
-};
+// const GalleryComponent = () => {
+//   return (
+//     <div>
+//       <ResponsiveGallery media={media} />
+//     </div>
+//   );
+// };
 
-export default GalleryComponent;
+// export default GalleryComponent;
