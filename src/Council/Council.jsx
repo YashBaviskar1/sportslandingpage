655744b019a3b9f46c1fdce7c52852f205fca1f7
Yadnesh Dinesh Bamne <110719904+YadneshBamne@users.noreply.github.com>
2024-12-20 20:28:54 +0530

    Update Council.jsx

--- src/Council/Council.jsx
+++ src/Council/Council.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState, useRef } from 'react';
-import ScrollReveal from 'scrollreveal';
 import './Council.css'; // assuming heads.css is in your src folder
 
 const Council = () => {
@@ -11,16 +10,7 @@ const Council = () => {
     window.scrollTo(0, 0);
   };
 
-  useEffect(() => {
-    // ScrollReveal animations
-    ScrollReveal({
-      distance: '60px',
-      duration: 2500,
-      delay: 400
-    });
-    ScrollReveal().reveal('h2', { delay: 100, origin: 'top' });
-    ScrollReveal().reveal('#team', { delay: 500, origin: 'bottom' });
-  }, []);
+
 
   // Tab content for each year
   const tabsContent = [
