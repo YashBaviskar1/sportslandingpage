cba6557a0210e1e64426489da396c8bc66b07035
Yadnesh Dinesh Bamne <110719904+YadneshBamne@users.noreply.github.com>
2024-12-20 22:22:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+
+   resolve: {
+    alias: {
+      'locomotive-scroll': 'locomotive-scroll/dist/locomotive-scroll.esm.js',
+    },
+  },
    optimizeDeps: {
       include: ['locomotive-scroll'],
   },
