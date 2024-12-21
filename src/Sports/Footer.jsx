3cf3c85c26072d83813e4644616d272b6ad01cbe
Yadnesh Dinesh Bamne <110719904+YadneshBamne@users.noreply.github.com>
2024-12-21 10:51:47 +0530

    Update Footer.jsx

--- src/Sports/Footer.jsx
+++ src/Sports/Footer.jsx
@@ -1,14 +1,47 @@
 import React from 'react';
-import { Container, Row, Col, Nav } from 'react-bootstrap';
-import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa'; 
-import './Footer.css'; 
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBCol,
+  MDBRow,
+  MDBIcon,
+  MDBBtn
+} from 'mdb-react-ui-kit';
 
-const Footer = () => {
+export default function Footer() {
   return (
-    <div class='footer'>
-    <h6>OJUS WEBDEV</h6>
-    </div>
-  );
-};
+    <MDBFooter className='bg-dark text-center text-white'>
+        {/* <section className='mb-4'>
+          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+            <MDBIcon fab icon='facebook-f' />
+          </MDBBtn>
 
-export default Footer;
+          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+            <MDBIcon fab icon='twitter' />
+          </MDBBtn>
+
+          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+            <MDBIcon fab icon='google' />
+          </MDBBtn>
+          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+            <MDBIcon fab icon='instagram' />
+          </MDBBtn>
+
+          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+            <MDBIcon fab icon='linkedin-in' />
+          </MDBBtn>
+
+          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+            <MDBIcon fab icon='github' />
+          </MDBBtn>
+        </section> */}
+
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2025 Copyright:
+        <a style={{ margin:'0px 20px' }} className='text-white' href='https://mdbootstrap.com/'>
+          Ojus Dev Team 2k25
+        </a>
+      </div>
+    </MDBFooter>
+  );
+}
