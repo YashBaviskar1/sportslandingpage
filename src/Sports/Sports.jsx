3b9ae302aff2ea2015ba49e57da9d8cb7114812f
Yadnesh Dinesh Bamne <110719904+YadneshBamne@users.noreply.github.com>
2024-12-22 15:43:15 +0530

    Update Sports.jsx

--- src/Sports/Sports.jsx
+++ src/Sports/Sports.jsx
@@ -3,8 +3,10 @@ import { Container, Row, Col, Image } from 'react-bootstrap';
 import Header from "./Header";
 import LogoFlexbox from './Department.jsx';
 import Events from './Events.jsx';
-import Footer from './Footer.jsx';
 import Gallery from './Gallery.jsx';
+import LocomotiveScroll from "locomotive-scroll";
+import "locomotive-scroll/dist/locomotive-scroll.css";
+import Footer from './Footer.jsx';
 import "./Sports.css";
 
 const Sports = () => {
@@ -15,19 +17,19 @@ const Sports = () => {
     <div className="sports">
       <Header />
       <div className='Department'>
-        <h1 className='headingCenter'>Who Will Win the Department Trophy Of 2025?</h1>
+        <h1 data-aos="zoom-in" className='headingCenter'>Who Will Win the Department Trophy Of 2025?</h1>
         <LogoFlexbox/>
       </div>
       <div className='events'>
-        <h1 className='headingCenter'>EVENTS</h1>
+        <h1 data-aos="zoom-in" className='headingCenter'>EVENTS</h1>
         <Events/> 
       </div>
       <div className='Gallery'>
-        <h1 className='headingCenter'>Gallery</h1>
+        <h1 data-aos="zoom-in" className='headingCenter'>Gallery</h1>
         <Gallery/>
       </div> 
+      <Footer></Footer>
     </div>
-      <Footer/>
     </div>
     </>
   )
