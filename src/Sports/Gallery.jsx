64ddea1d9a430d6f2882251e62890c6164254661
Yadnesh Dinesh Bamne <110719904+YadneshBamne@users.noreply.github.com>
2024-12-22 19:47:04 +0530

    Update Gallery.jsx

--- src/Sports/Gallery.jsx
+++ src/Sports/Gallery.jsx
@@ -4,18 +4,18 @@ import {
   MDBCol,
   MDBRow,
 } from 'mdb-react-ui-kit';
-export default function App() {
+
+export default function Gallery() {
   return (
-    <MDBRow style={{ padding:'80px'}}>
+    <MDBRow>
       <MDBCol lg={4} md={12} className='mb-4 mb-lg-0'>
-        <img 
-          data-aos="fade-right"
+        <img
           src='https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp'
           className='w-100 shadow-1-strong rounded mb-4'
           alt='Boat on Calm Water'
         />
+
         <img
-          data-aos="fade-up"
           src='https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp'
           className='w-100 shadow-1-strong rounded mb-4'
           alt='Wintry Mountain Landscape'
@@ -24,14 +24,12 @@ export default function App() {
 
       <MDBCol lg={4} className='mb-4 mb-lg-0'>
         <img
-          data-aos="fade-down"
           src='https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp'
           className='w-100 shadow-1-strong rounded mb-4'
           alt='Mountains in the Clouds'
         />
 
         <img
-          data-aos="fade-up"
           src='https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp'
           className='w-100 shadow-1-strong rounded mb-4'
           alt='Boat on Calm Water'
@@ -40,14 +38,12 @@ export default function App() {
 
       <MDBCol lg={4} className='mb-4 mb-lg-0'>
         <img
-          data-aos="fade-left"
           src='https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp'
           className='w-100 shadow-1-strong rounded mb-4'
           alt='Waves at Sea'
         />
 
         <img
-          data-aos="fade-up"
           src='https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp'
           className='w-100 shadow-1-strong rounded mb-4'
           alt='Yosemite National Park'
