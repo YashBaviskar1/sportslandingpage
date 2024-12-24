ff145bc41052365321925eb3e304f851e0ff08fe
Yadnesh Dinesh Bamne <110719904+YadneshBamne@users.noreply.github.com>
2024-12-24 16:58:46 +0530

    Update Events.jsx

--- src/Sports/Events.jsx
+++ src/Sports/Events.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './Events.css';
-import sport1 from "../assets/FOOTBALL.png";
+import sport1 from "../assets/BADMINTON.png";
 import sport2 from "../assets/FOOTBALL.png";
 import sport3 from "../assets/FOOTBALL.png";
 
