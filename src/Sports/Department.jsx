23e2b70c810fba9620f6a9cf1a0da4312850edaa
Yadnesh Dinesh Bamne <110719904+YadneshBamne@users.noreply.github.com>
2024-12-25 12:44:02 +0530

    Update Department.jsx

--- src/Sports/Department.jsx
+++ src/Sports/Department.jsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import aimlsa from "../assets/aimlsa.png";
-import csa from "../assets/csa.png";
-import dsa from "../assets/dsa.png";
-import itsa from "../assets/itsa.png";
 import './Department.css';
 const LogoFlexbox = () => {
   return (
@@ -17,12 +13,13 @@ const LogoFlexbox = () => {
       borderRadius:"45px",
       margin:"auto",
     }}>
-      <img src={aimlsa} alt="Logo 1" style={{ width: '130px', height: 'auto' }} />
-      <img src={csa} alt="Logo 2" style={{ width: '130px', height: 'auto' }} />
-      <img src={dsa} alt="Logo 3" style={{ width: '130px', height: 'auto' }} />
-      <img src={itsa} alt="Logo 4" style={{ width: '130px', height: 'auto' }} />
+      <img src="https://res.cloudinary.com/dyhby7bo9/image/upload/v1734699257/OJUSPHOTOS/s7kpjfjof8jsckpqihsz.png" alt="Logo 1" style={{ width: '130px', height: 'auto' }} />
+      <img src="https://res.cloudinary.com/dyhby7bo9/image/upload/v1734699263/OJUSPHOTOS/hmqlfhlrv92drregbyhg.png" alt="Logo 2" style={{ width: '130px', height: 'auto' }} />
+      <img src="https://res.cloudinary.com/dyhby7bo9/image/upload/v1734699266/OJUSPHOTOS/j1fi3twa2rx3x3lhuwa8.png" alt="Logo 3" style={{ width: '130px', height: 'auto' }} />
+      <img src="https://res.cloudinary.com/dyhby7bo9/image/upload/v1734699276/OJUSPHOTOS/vbd00sgou0ychslyokto.png" alt="Logo 4" style={{ width: '130px', height: 'auto' }} />
       <img src="https://res.cloudinary.com/dyhby7bo9/image/upload/v1734699277/OJUSPHOTOS/gvuiwwildkcuc36sdtnz.png" alt="Logo 5" style={{ width: '130px', height: 'auto' }} />
       <img src="https://res.cloudinary.com/dyhby7bo9/image/upload/v1735038785/cu8hlpt8rqt5o4iviahc.png" alt="Logo 5" style={{ width: '130px', height: 'auto' }} />
+      <img src="https://res.cloudinary.com/dyhby7bo9/image/upload/v1735110819/OJUSPHOTOS/hovsnh5gpb1ogo92wiqa.png" alt="Logo 5" style={{ width: '130px', height: 'auto' }} />
     </div>
   );
 };
