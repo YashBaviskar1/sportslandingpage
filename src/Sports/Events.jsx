611c792037d21b1bbc4e273ff1b4e6c2c0d296b5
Yadnesh Dinesh Bamne <110719904+YadneshBamne@users.noreply.github.com>
2024-12-25 13:49:07 +0530

    Update Events.jsx

--- src/Sports/Events.jsx
+++ src/Sports/Events.jsx
@@ -1,47 +1,9 @@
 import React, { useState } from 'react';
 import './Events.css';
-import sport1 from "../assets/BADMINTON.png";
-import sport2 from "../assets/FOOTBALL.png";
-import sport3 from "../assets/VOLLEYBALL.png";
-import sport5 from "../assets/BOXCRICKET.png";
-import sport6 from "../assets/CARROM2.png";
-import sport7 from "../assets/CARROM.png";
-import sport8 from "../assets/TABLETENNIS.png";
-import sport9 from "../assets/TABLETENNIS2.png";
-
-import sport11 from "../assets/BADMINTON2.png";
-import sport12 from "../assets/THROWBALL.png";
-
+import { Link } from 'react-router-dom';
 
 const Events = () => {
   const [selectedDay, setSelectedDay] = useState('day1');
-
-  const eventImages = {
-    day1: [
-      sport5,
-      sport2,
-    ],
-    day2: [
-   sport2,
-   sport12,
-   sport3,
-   sport1,
-    ],
-    day3: [
-      sport1,
-      sport5,
-    ],
-    day4: [
-     sport11,
-     sport9,
-     sport1,
-    ],
-    day5: [
-   sport6,
-   sport5,
-   sport3,
-    ],
-  };
 
   return (
     <div className="event-tabs">
@@ -57,9 +19,69 @@ const Events = () => {
         ))}
       </div>
       <div className="image-container">
-        {eventImages[selectedDay].map((img, index) => (
-          <img key={index} src={img} alt={`Event for ${selectedDay}`} />
-        ))}
+        {selectedDay === 'day1' && (
+          <div>
+            <Link to="/box-cricket">
+              <img src="https://res.cloudinary.com/dyhby7bo9/image/upload/v1735114391/OJUSPHOTOS/txhagfnx2aopmb6xmeod.png" alt="Cricket" />
+            </Link>
+            
+            <Link to="/box-cricket">
+              <img src="https://res.cloudinary.com/dyhby7bo9/image/upload/v1735114391/OJUSPHOTOS/txhagfnx2aopmb6xmeod.png" alt="Tug of War" />
+            </Link>
+          </div>
+        )}
+        {selectedDay === 'day2' && (
+          <div>
+            <Link to="/box-cricket">
+              <img src="https://res.cloudinary.com/dyhby7bo9/image/upload/v1735114393/OJUSPHOTOS/r00itn3tjc2xqe94egmd.png" alt="Football" />
+            </Link>
+            <Link to="/box-cricket">
+              <img src="https://res.cloudinary.com/dyhby7bo9/image/upload/v1734699286/OJUSPHOTOS/klwengfb45tuk7enfz85.png" alt="Dodgeball" />
+            </Link>
+            <Link to="/box-cricket">
+              <img src="https://res.cloudinary.com/dyhby7bo9/image/upload/v1734699286/OJUSPHOTOS/klwengfb45tuk7enfz85.png" alt="Volleyball" />
+            </Link>
+            <Link to="/box-cricket">
+              <img src="https://res.cloudinary.com/dyhby7bo9/image/upload/v1735114391/OJUSPHOTOS/txhagfnx2aopmb6xmeod.png" alt="Tug of War(Girls)" />
+            </Link>
+          </div>
+        )}
+        {selectedDay === 'day3' && (
+          <div>
+            <Link to="/box-cricket">
+              <img src="https://res.cloudinary.com/dyhby7bo9/image/upload/v1734699263/OJUSPHOTOS/tv4uivtvvrr5ipzcc7nc.png" alt="Kabbadi" />
+            </Link>
+            <Link to="/box-cricket">
+              <img src="https://res.cloudinary.com/dyhby7bo9/image/upload/v1734699264/OJUSPHOTOS/rbhv5gcw8xyi2r5bmmmj.png" alt="Box-Cricket(Girls)" />
+            </Link>
+          </div>
+        )}
+        {selectedDay === 'day4' && (
+          <div>
+            <Link to="/box-cricket">
+              <img src="https://res.cloudinary.com/dyhby7bo9/image/upload/v1734699263/OJUSPHOTOS/tv4uivtvvrr5ipzcc7nc.png" alt="Badminton" />
+            </Link>
+            <Link to="/box-cricket">
+              <img src="https://res.cloudinary.com/dyhby7bo9/image/upload/v1735114043/OJUSPHOTOS/gp0dmtn5dsddcllhedm9.png" alt="Table Tennis" />
+            </Link>
+            <Link to="/box-cricket">
+              <img src="https://res.cloudinary.com/dyhby7bo9/image/upload/v1735114067/OJUSPHOTOS/ezhcy7gjx1kurg34lwj5.png" alt="Chess" />
+            </Link>
+          </div>
+        )}
+        {selectedDay === 'day5' && (
+          <div>
+            <Link to="/box-cricket">
+              <img src="https://res.cloudinary.com/dyhby7bo9/image/upload/v1735114166/OJUSPHOTOS/a8sjee5xjohr0cetmdgq.png" alt="Carrom" />
+            </Link>
+            <Link to="/box-cricket">
+              <img src="https://res.cloudinary.com/dyhby7bo9/image/upload/v1734699264/OJUSPHOTOS/rbhv5gcw8xyi2r5bmmmj.png" alt="Box Cricket (boys)" />
+            </Link>
+            <Link to="/box-cricket">
+              <img src="https://res.cloudinary.com/dyhby7bo9/image/upload/v1735114536/OJUSPHOTOS/qbgrh0cgoutdh7c3eokp.png" alt="Throwball (girls)" />
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
