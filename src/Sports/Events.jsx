40059389086a8d55e87759e9995e8aa88d8d50c0
YashBaviskar1 <104298817+YashBaviskar1@users.noreply.github.com>
2024-12-28 20:27:10 +0530

    Update Events.jsx

--- src/Sports/Events.jsx
+++ src/Sports/Events.jsx
@@ -52,7 +52,7 @@ const Events = () => {
               <img src="https://res.cloudinary.com/dyhby7bo9/image/upload/v1735322455/New%20Sports%20Card/rpdabafu34vlkdqjdbbv.png" alt="Kabbadi" />
             </Link>
             <Link to="/box-cricket(girls)/details">
-              <img src="https://res.cloudinary.com/dyhby7bo9/image/upload/v1735395931/BOX_CRICKET_2_pfr0p9.png.png" alt="Box-Cricket(Girls)" />
+              <img src="https://res.cloudinary.com/dyhby7bo9/image/upload/v1735395931/BOX_CRICKET_2_pfr0p9.png" alt="Box-Cricket(Girls)" />
             </Link>
           </div>
         )}
