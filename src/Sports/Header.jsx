474adb976c00af66449b67d65f7d884b57caab13
YashBaviskar1 <104298817+YashBaviskar1@users.noreply.github.com>
2024-12-28 18:09:12 +0530

    Update Header.jsx

--- src/Sports/Header.jsx
+++ src/Sports/Header.jsx
@@ -106,12 +106,12 @@ const HeroSection = () => {
 
       {/* Full-Screen Sliding Menu */}
       <ul className={`menu1 ${menuOpen ? "open" : ""}`}>
-        <li>
-          <Link to="/events">Events</Link>
-        </li>
-        <li>
-          <Link to="/gallery">Gallery</Link>
-        </li>
+      <li>
+      <a href="#events" onClick={() => setMenuOpen(false)}>Events</a>
+    </li>
+    <li>
+    <a href="#gallery" onClick={() => setMenuOpen(false)}>Gallery</a>
+    </li>  
         <li>
           <Link to="/schedule">Schedule</Link>
         </li>
